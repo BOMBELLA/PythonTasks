162177b403ea150233e481f1f207287425678a16
BOMBELLA <nmasionyena@gmail.com>
2026-04-20 10:38:20 +0100

    first multiplication

--- ClassWork/customer.py
+++ ClassWork/customer.py
@@ -1,1 +1,19 @@
+// look at it from the view of of a shop owner thats running a discount sales.The more you purchase from him, the more discount you get.So anyone that buys something between 1 to 10,000 gets a 5% discount one the purchase. Also any one that buys 10,000 to 50,000 gets a 10% discount and anyone that buys anything from 50,000 and abovoe gets a 20% discount.//
+
+
+
+
+
+
+
+
+
+amountone = 1,000 to 10,000
+
+amounttwo = 10,000 to 50,000
+
+
+amountthree = 60,000 and above
+
+
 
