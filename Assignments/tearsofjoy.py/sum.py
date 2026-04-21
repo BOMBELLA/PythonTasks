6b99231672de6ad57d36e0f72be3aadf1c949b0c
BOMBELLA <nmasionyena@gmail.com>
2026-04-22 00:51:20 +0100

    sum

---
+++ Assignments/tearsofjoy.py/sum.py
@@ -0,0 +1,16 @@
+# step 1 sum up numbers 1 to 100
+# step 2 use the sum to calculate the number
+# step 3 using for loop 
+# step 4 output should be horizontal
+
+
+
+
+
+sum_of_numbers  = 0
+
+for counter in range(1,101):
+
+       sum_of_numbers = sum_of_numbers + counter
+
+print(sum_of_numbers)
