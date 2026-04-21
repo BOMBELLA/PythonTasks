e810dc9abbd502cf095afde5d6586546a5048d4e
BOMBELLA <nmasionyena@gmail.com>
2026-04-22 00:24:41 +0100

    print even numbers

---
+++ Assignments/tearsofjoy.py/evennumber.py
@@ -0,0 +1,14 @@
+# Step 1: Use a for loop to count even numbers
+
+# Step 2: the initial count starts from 2 and terminates at 20
+# Step 3: then print out in a horizontal way
+
+
+
+
+
+
+for digit in range(2, 21):
+       if digit % 2 == 0:
+              print(digit , end=" ")
+     
