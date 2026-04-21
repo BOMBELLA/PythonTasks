b4741924ddddfbab99607d9e7daebfd2285de45f
BOMBELLA <nmasionyena@gmail.com>
2026-04-21 23:22:17 +0100

    loopnumbers

---
+++ Assignments/tearsofjoy.py/loop.py
@@ -0,0 +1,19 @@
+# step 1 : using for loop, 
+# step 2 : count to 10 and 
+# step 3 : it should be on a new line
+
+
+
+
+       
+
+
+for count in range(1,11):
+
+
+       print(count)
+       
+       
+       
+             
+       
