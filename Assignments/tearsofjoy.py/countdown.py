7b9d403e735e2095023902b5c54f9564911c336e
BOMBELLA <nmasionyena@gmail.com>
2026-04-22 03:54:40 +0100

    mutiplicationseven

---
+++ Assignments/tearsofjoy.py/countdown.py
@@ -0,0 +1,8 @@
+# Step 1 
+for count in range (10, 1, -1) :
+
+       print(count, end=" ");
+
+print("Blast off !!!")
+
+            
---
+++ Assignments/tearsofjoy.py/multiplication_seven.py
@@ -0,0 +1,14 @@
+#step 1:Using a multiplication table
+#step 2:print the 7 times table using for loop
+
+
+
+
+
+for number in range(1,13):
+     
+
+       print("7  X  ",number, " = ", 7 * number)
+
+
+       
