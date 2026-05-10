49c5ce68e7f6de6d303e5fd48e79c79e2a2e2e2d
BOMBELLA <nmasionyena@gmail.com>
2026-05-10 17:46:26 +0100

    safe division

---
+++ MR CHINEDU SNACKS/WEEKEND FUN/two_integers.py
@@ -0,0 +1,15 @@
+x = int(input("Enter the first number : "))
+
+y = int(input("Enter the second number : "))
+
+result = x / y
+
+if(y!= 0):
+
+       print(result)
+
+if(y == 0):
+
+       print("cannot divide by zero")
+
+
